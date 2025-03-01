f75d97b85d15dd51a0e5e3e1c34127ab5efd9870
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-02-28 22:17:59 -0500

    Agregando ambientes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -13,14 +14,13 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class AuthService {
-  private BASE_URL: string = 'http://localhost:8080/api/v1/auth'; // api
 
   constructor(private http: HttpClient) {}
 
   // login de usuarios
   login(username: string, password: string): Observable<any> {
     return this.http.post(
-      `${this.BASE_URL}/login`,
+      `${environment.API_URL}/auth/login`,
       { username, password },
       httpOptions
     );
@@ -29,7 +29,7 @@ export class AuthService {
   // registro de usuarios
   register(name: string, username: string, password: string): Observable<any> {
     return this.http.post(
-      `${this.BASE_URL}/register`,
+      `${environment.API_URL}/auth/register`,
       {
         name,
         username,
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private BASE_URL: string = 'http://localhost:8080/api/v1/users'; // api
 
   constructor(private http: HttpClient) {}
 
   // obtener el usuario autenticado
   getAuthenticatedUser(): Observable<any> {
-    return this.http.get(`${this.BASE_URL}/me`);
+    return this.http.get(`${environment.API_URL}/users/me`);
   }
 }
